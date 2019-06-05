7e5a2d8f7dafaceeb24380104a2d93ce0a6a39df
Pudding <s.kokuryo@gmail.com>
2019-06-05 13:24:01 +0900

    Remove unused roles

--- roles/xkeysnail/templates/start-xkeysnail.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ -x {{ xkeysnail_path.stdout }} ]; then
-    xhost +SI:localuser:xkeysnail
-    sudo -u xkeysnail DISPLAY=:1 {{ xkeysnail_path.stdout }} {{ xkeysnail_config_dir }}/config.py &
-fi
