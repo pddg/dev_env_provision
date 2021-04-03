bbf867aa9ac81b2866e6ffe26602a2d6fe08fd4d
Pudding <s.kokuryo@gmail.com>
2021-04-03 18:07:10 +0900

    do not use pipenv

--- Makefile
+++ Makefile
@@ -1,13 +1,10 @@
 SHELL := /bin/bash
 
 PLAYBOOK_DIR := playbooks
-PLAYBOOK_CMD := pipenv run ansible-playbook
+PLAYBOOK_CMD := ansible-playbook
 
 xcode:
 	xcode-select --install
-
-zlib_mojave:
-	sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
 
 brew:
 	bash scripts/install_brew.sh
@@ -24,4 +21,4 @@ init:
 tools:
 	$(PLAYBOOK_CMD) -K $(PLAYBOOK_DIR)/tools.yml $(args)
 
-.PHONY: xcode zlib_mojave brew ansible macos ubuntu init tools ;
+.PHONY: xcode brew ansible macos ubuntu init tools ;
--- scripts/install_ansible.sh
+++ scripts/install_ansible.sh
@@ -2,20 +2,19 @@
 
 set -o pipefail
 
-if [ "$(uname)" == 'Darwin'  ]; then
-    if type python3 >/dev/null 2>&1; then
-        brew install python3
-    else
-        echo "Python3 is installed."
-    fi
-    sudo pip3 install pipenv
+if type ansible > /dev/null 2>&1; then
+    echo "ansible is installed."
+    ansible --version
 else
-    # RedHat distributions are not supported
-    sudo apt update
-    sudo apt install python3 python3-pip
-    sudo pip3 install pipenv
+    if [ "$(uname)" == 'Darwin'  ]; then
+        brew install ansible
+    else
+        # RedHat distributions are not supported
+        sudo apt update && \
+        sudo apt install -y --no-install-recommends \
+            python3 \
+            python3-pip \
+            ansible
+    fi
+    ansible --version
 fi
-
-# Install dependencies
-pipenv install
-
